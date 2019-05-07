86a288064b171468f5afed06884379dc8763f083
yonykikok <jonathan.n.haedo@gmail.com>
2019-05-06 22:07:31 -0300

    Clase 07 API REST

---
+++ Programacion/Clase07/clases/usuarioApi.php
@@ -0,0 +1,98 @@
+<?php
+require_once 'usuario.php';
+require_once 'IApiUsable.php';
+
+class usuarioApi extends usuario implements IApiUsable
+{
+ 	public function TraerUno($request, $response, $args) {
+     	$id=$args['id'];
+    	$elUsuario=usuario::TraerUnUsuario($id);
+     	$newResponse = $response->withJson($elUsuario, 200);  
+    	return $newResponse;
+    }
+     public function TraerTodos($request, $response, $args) {
+      	$todosLosUsuarios=usuario::TraerTodoLosUsuarios();
+     	$newResponse = $response->withJson($todosLosUsuarios, 200);  
+    	return $newResponse;
+    }
+      public function CargarUno($request, $response, $args) {
+     	 $ArrayDeParametros = $request->getParsedBody();
+        $usuario= $ArrayDeParametros['usuario'];
+        $password= $ArrayDeParametros['password'];
+        
+        $miUsuario = new usuario();
+        $miUsuario->password=$password;
+        $miUsuario->usuario=$usuario;
+        $miUsuario->InsertarElUsuarioParametros();
+
+        $archivos = $request->getUploadedFiles();
+        $destino="./fotos/";
+        //var_dump($archivos);
+        //var_dump($archivos['foto']);
+
+        $nombreAnterior=$archivos['foto']->getClientFilename();
+        $extension= explode(".", $nombreAnterior)  ;
+        //var_dump($nombreAnterior);
+        $extension=array_reverse($extension);
+
+        $archivos['foto']->moveTo($destino.$usuario.".".$extension[0]);
+        $response->getBody()->write("se guardo el usuario");
+
+        return $response;
+    }
+      public function BorrarUno($request, $response, $args) {
+     	$ArrayDeParametros = $request->getParsedBody();
+     	$id=$ArrayDeParametros['id'];
+     	$usuario= new usuario();
+     	$usuario->id=$id;
+     	$cantidadDeBorrados=$usuario->BorrarUsuario();
+
+     	$objDelaRespuesta= new stdclass();
+	    $objDelaRespuesta->cantidad=$cantidadDeBorrados;
+	    if($cantidadDeBorrados>0)
+	    	{
+	    		 $objDelaRespuesta->resultado="algo borro!!!";
+	    	}
+	    	else
+	    	{
+	    		$objDelaRespuesta->resultado="no Borro nada!!!";
+	    	}
+	    $newResponse = $response->withJson($objDelaRespuesta, 200);  
+      	return $newResponse;
+    }
+     
+     public function ModificarUno($request, $response, $args) {
+     	//$response->getBody()->write("<h1>Modificar  uno</h1>");
+		 $ArrayDeParametros = $request->getParsedBody();
+			
+	    //var_dump($ArrayDeParametros);    	
+	    $miUsuario = new usuario();
+	    $miUsuario->id=$ArrayDeParametros['id'];
+	    $miUsuario->password=$ArrayDeParametros['password'];
+		$miUsuario->usuario=$ArrayDeParametros['usuario'];	
+		
+	   	$resultado =$miUsuario->ModificarUsuarioParametros();
+	   	$objDelaRespuesta= new stdclass();
+		//var_dump($resultado);
+		$objDelaRespuesta->resultado=$resultado;
+		return $response->withJson($objDelaRespuesta, 200);		
+	}
+	public function Login($request, $response, $args) {
+		$ArrayDeParametros = $request->getParsedBody();
+
+	   $miUsuario = new usuario();
+	   $miUsuario->id=$ArrayDeParametros['id'];
+	   $miUsuario->password=$ArrayDeParametros['password'];
+	   $miUsuario->usuario=$ArrayDeParametros['usuario'];	
+	   
+	   $resultado =$miUsuario->TraerUnUsuarioPorLogin($ArrayDeParametros['usuario'],$ArrayDeParametros['password']);
+	   var_dump($resultado) ;
+		 die();	
+		  $objDelaRespuesta= new stdclass();
+
+	   $objDelaRespuesta->resultado=$resultado;
+	   return $response->withJson($objDelaRespuesta, 200);		
+   }
+
+
+}
---
+++ Programacion/Clase07/index.php
@@ -0,0 +1,34 @@
+<?php
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+require '/composer/vendor/autoload.php';
+require '/clases/AccesoDatos.php';
+require '/clases/usuarioApi.php';
+
+
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+
+$app = new \Slim\App(["settings" => $config]);
+
+$app->group('/usuario', function () {
+ 
+  $this->get('/', \usuarioApi::class . ':traerTodos');
+ 
+  $this->get('/{id}', \usuarioApi::class . ':traerUno');
+
+  $this->post('/', \usuarioApi::class . ':CargarUno');
+
+  $this->delete('/', \usuarioApi::class . ':BorrarUno');
+
+  $this->put('/', \usuarioApi::class . ':ModificarUno');
+     
+});
+
+$app->group('/login',function(){
+
+  $this->post('/', \usuarioApi::class . ':Login');
+});
+
+$app->run();
